f676d24d355dcc57afcc3e39a3899fc63b38ee14
ocular-d <sven@ocular-d.tech>
2020-03-21 15:11:28 +0100

    chore(action): first files for test

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eu
+
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+
+#USE_QUIET_MODE="$1"
+#USE_VERBOSE_MODE="$2"
+#CONFIG_FILE="$3"
+#FOLDER_PATH="$4"
+
+#echo -e "${BLUE}USE_QUIET_MODE: $1${NC}"
+#echo -e "${BLUE}USE_VERBOSE_MODE: $2${NC}"
+#echo -e "${BLUE}FOLDER_PATH: $4${NC}"
+
+echo -e "${YELLOW}=========================> REMARK CHECK <=========================${NC}"
+remark
