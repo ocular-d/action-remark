d50da3ec33e996d03aeccbb78f1124ab29f66a7c
ocular-d <sven@ocular-d.tech>
2020-04-08 12:42:53 +0200

    ci(test): play with find

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,4 +17,5 @@ BLUE='\033[0;34m'
 #echo -e "${BLUE}FOLDER_PATH: $4${NC}"
 
 echo -e "${YELLOW}=========================> REMARK CHECK <=========================${NC}"
-remark --frail md
+#remark --frail md
+find -name \*.md -exec remark --frail {} \;
