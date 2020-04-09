b3643b8847cd8628e5c8c09bbb78dc2c79f0694a
ocular-d <sven@ocular-d.tech>
2020-04-09 10:27:16 +0200

    code(entrypoint): plagying

--- test.sh
+++ test.sh
@@ -7,6 +7,9 @@ GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 
+FILE=error.log
+
+
 #USE_QUIET_MODE="$1"
 #USE_VERBOSE_MODE="$2"
 #CONFIG_FILE="$3"
@@ -17,19 +20,16 @@ BLUE='\033[0;34m'
 #echo -e "${BLUE}FOLDER_PATH: $4${NC}"
 
 echo -e "${YELLOW}=========================> REMARK CHECK <=========================${NC}"
-remark --no-color -q md 2>&1 | tee error.log
+./node_modules/.bin/remark --no-color -q md 2>&1 | tee error.log
+
 if [ -s "$FILE" ]; then
     echo "$FILE exist"
     echo "there was an error"
-    echo ::set-output name=status::'There was an issue.'
     exit 1
 else
     echo "great, all good"
-    echo ::set-output name=status::'Success'
     exit 0
 fi
-
-
 
 #remark --frail md
 #find -name \*.md -exec remark --frail {} \;
