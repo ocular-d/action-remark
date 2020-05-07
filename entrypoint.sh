1ac37f407fc177b7ed8916a381f8017a4e6a529c
sven <sven@so36.net>
2020-05-07 16:14:57 +0200

    chore(code): update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ RED='\033[0;31m'
 FILE=error.log
 
 echo -e "${YELLOW}=========================> REMARK CHECK <=========================${NC}"
-./node_modules/.bin/remark --no-color -q $1 2>&1 | tee error.log
+remark --no-color -q $1 2>&1 | tee error.log
 if [ -s "$FILE" ]; then
     echo -e "${RED}Ooh, there was an error${NC}"
     echo ::set-output name=exit_code::1
